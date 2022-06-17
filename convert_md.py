24ffdb6bd7f58944d30323d7c1244041a328b708
pistack <pistatex@yonsei.ac.kr>
2022-06-18 04:23:17 +0900

    add benchmark example to test some routine

--- convert_md.py
+++ convert_md.py
@@ -1,12 +1,12 @@
 import subprocess
 
-example = ['IRF', 'Rate_eq', 'basic', 'broad', 'dmp_osc']
+example = ['IRF', 'Rate_eq', 'basic', 'broad', 'dmp_osc', 'BenchMark']
 
 for e in example:
     subprocess.run(['jupyter', 'nbconvert', '--to', 'markdown',
                     f'{e}.ipynb', '--output',
                     f'{e}.md'])
     subprocess.run(['rm', '-rf',
-                    f'../TRXAS-pre-fit-pack/docs/source/{e}_files/'])
+                    f'../TRXASprefitpack/docs/source/{e}_files/'])
     subprocess.run(['mv', '-f', f'{e}.md', f'{e}_files',
-                    '../TRXAS-pre-fit-pack/docs/source/'])
+                    '../TRXASprefitpack/docs/source/'])
