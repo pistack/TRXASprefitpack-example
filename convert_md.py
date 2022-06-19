952f6cf2cca87794abf0852dab420b6074613654
pistack <pistatex@yonsei.ac.kr>
2022-06-19 17:39:49 +0900

    update notebook

--- convert_md.py
+++ convert_md.py
@@ -1,6 +1,6 @@
 import subprocess
 
-example = ['IRF', 'Rate_eq', 'basic', 'broad', 'dmp_osc', 'BenchMark']
+example = ['basic', 'IRF', 'Rate_eq', 'basic', 'broad', 'dmp_osc', 'BenchMark']
 
 for e in example:
     subprocess.run(['jupyter', 'nbconvert', '--to', 'markdown',
